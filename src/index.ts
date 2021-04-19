4f619096737a9067dac35ff8917d7e2fee7e88df
Georgian Stan <geeostan8@gmail.com>
2021-04-19 18:37:01 +0300

    feat: implement themeing, custom css class & theme, callbacks and transition

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,13 @@ import template from './index.pug';
 /**
  * * Types
  */
-import { DefaultOptions, MenuItem, Options } from './@types/interface';
+import {
+  CoreOptions,
+  DefaultOptions,
+  MenuItem,
+  ConfigurableOptions,
+  Options,
+} from './@types/interface';
 
 interface State {
   style: any;
@@ -16,9 +22,18 @@ interface State {
 
 export default class VanillaContextMenu {
   // * private vars
-  #state: State = { style, menuItems: [] };
-  #defaultOptions: DefaultOptions = {};
+  #state: State = { style, menuItems: [] }; // ? state for pug template
 
+  #coreOptions: CoreOptions = {
+    transformOrigin: ['top', 'left'], // ? show not be
+  };
+
+  #defaultOptions: DefaultOptions = {
+    theme: 'black',
+    transitionDuration: 200,
+  };
+
+  // ? will be populated in constructor
   //@ts-ignore
   #options: Options = {};
 
@@ -89,14 +104,75 @@ export default class VanillaContextMenu {
   };
 
   // *
+  #applyStyleOnContextMenu = (
+    contextMenu: HTMLElement,
+    outOfBoundsOnX: boolean,
+    outOfBoundsOnY: boolean,
+  ) => {
+    // ? transition duration
+    contextMenu.style.transitionDuration = `${
+      this.#options.transitionDuration
+    }ms`;
+
+    // ? set the transition origin based on it's position
+    let transformOrigin: [string, string] = Array.from(
+      this.#options.transformOrigin,
+    ) as [string, string];
+
+    outOfBoundsOnX && (transformOrigin[1] = 'right');
+    outOfBoundsOnY && (transformOrigin[0] = 'bottom');
+
+    contextMenu.style.transformOrigin = transformOrigin.join(' ');
+
+    // ? apply theme or custom css style
+    if (this.#options.customThemeClass) {
+      contextMenu.classList.add(this.#options.customThemeClass);
+    } else {
+      contextMenu.classList.add(
+        style[`context-menu--${this.#options.theme}-theme`],
+      );
+    }
+
+    this.#options.customClass &&
+      contextMenu.classList.add(this.#options.customClass);
+  };
+
+  #bindCallbacks = (contextMenu: HTMLElement) => {
+    this.#options.menuItems.forEach((menuItem: MenuItem, index: number) => {
+      if (menuItem === 'hr') {
+        return;
+      }
+
+      const htmlEl: HTMLElement = contextMenu.children[index] as HTMLElement;
+
+      htmlEl.onclick = () => {
+        menuItem.callback();
+
+        // ? global value for all menu items, or the individual option or false
+        const preventCloseOnClick: boolean =
+          menuItem.preventCloseOnClick ??
+          this.#options.preventCloseOnClick ??
+          false;
+
+        if (!preventCloseOnClick) {
+          this.#removeExistingContextMenu();
+        }
+      };
+    });
+  };
+
+  // *
   #onShowContextMenu = (event: MouseEvent) => {
     event.preventDefault();
 
+    // ? the current context menu should disappear when a new one is displayed
     this.#removeExistingContextMenu();
 
+    // ? build and show on ui
     const contextMenu: HTMLElement = this.#buildContextMenu();
     document.querySelector('body').append(contextMenu);
 
+    // ? set the position
     const { clientX: mouseX, clientY: mouseY } = event;
 
     const { normalizedX, normalizedY } = this.#normalizePozition(
@@ -107,6 +183,24 @@ export default class VanillaContextMenu {
 
     contextMenu.style.top = `${normalizedY}px`;
     contextMenu.style.left = `${normalizedX}px`;
+
+    // ? apply the css configurable style
+    this.#applyStyleOnContextMenu(
+      contextMenu,
+      mouseX !== normalizedX,
+      mouseY !== normalizedY,
+    );
+
+    // ? disable context menu for it
+    contextMenu.oncontextmenu = (e) => e.preventDefault();
+
+    // ? bind the callbacks on each option
+    this.#bindCallbacks(contextMenu);
+
+    // ? make it visible but wait an event loop to pass
+    setTimeout(() => {
+      contextMenu.classList.add(style['visible']);
+    });
   };
 
   /**
@@ -122,9 +216,8 @@ export default class VanillaContextMenu {
     this.#removeExistingContextMenu();
   };
 
-  constructor(options: Options) {
-    // ? extend default options and bind the menu items inside the state for pug template
-    Object.assign(this.#options, options);
+  constructor(configurableOptions: ConfigurableOptions) {
+    this.updateOptions(configurableOptions);
 
     this.#state.menuItems = this.#options.menuItems;
 
@@ -133,5 +226,25 @@ export default class VanillaContextMenu {
 
     // ? add a click event listener to create a modal effect for the context menu and close it if the user clicks outside of it
     document.addEventListener('click', this.#onDocumentClick);
+  }
+
+  /**
+   * * Public methods (API)
+   */
+
+  /**
+   * * Remove all the event listeners that were registered for this feature
+   */
+  off() {
+    document.removeEventListener('click', this.#onDocumentClick);
+  }
+
+  updateOptions(configurableOptions: ConfigurableOptions) {
+    // ? extend default options and bind the menu items inside the state for pug template
+    Object.assign(this.#options, this.#defaultOptions);
+    Object.assign(this.#options, configurableOptions);
+    Object.assign(this.#options, this.#coreOptions);
+
+    this.#state.menuItems = this.#options.menuItems;
   }
 }
