05be0c910fd44bda9e3259edf92b3e65bb8df4f2
GeorgianStan <georgian.stan8@gmail.com>
2022-10-11 17:14:25 +0300

    chore: remove cypress
    Declutter the project by removing unnecessary code and libraries.
    Remove cypress as is not used at the moment for testing.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '.git', '/dist/'],
   setupFiles: ['./setupJest.ts'],
-  modulePathIgnorePatterns: ['cypress'],
   automock: false,
 };
