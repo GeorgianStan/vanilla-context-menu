3d030ba97bbd6da8c350983130f03538ee4e38d4
GeorgianStan <georgian.stan8@gmail.com>
2023-06-09 09:02:52 +0300

    feat: bind contextmenu event listener on parent element
    Implement a function that can be used to remove the custom element and
    the contextmenu event listener from the parent element.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import style from './index.scss';
 
 class VanillaContextMenu extends HTMLElement {
+  // private properties
   #shadow: ShadowRoot;
 
+  // private methods
+  #_onContextMenu(event: MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  // To remove and event listener, the same options that were send to 'addEventListener' functions must be send. Since 'bind' creates a new function, it's necessary to store the reference to this new function in a variable.
+  #onContextMenu: (event: MouseEvent) => void = this.#_onContextMenu.bind(this);
+
+  /** Removes the custom element and the 'contextmenu' event attached to the parent element. */
+  #remove() {
+    this.parentElement.removeEventListener('contextmenu', this.#onContextMenu);
+    this.remove();
+  }
+
+  // life-cycle methods
   constructor() {
     super();
     this.#shadow = this.attachShadow({ mode: 'open' });
     style.use({ target: this.#shadow });
+
+    this.parentElement.addEventListener('contextmenu', this.#onContextMenu);
   }
 }
 
